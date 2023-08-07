9e27de967a1433dc1dae5e8288705fd54c165e32
Ailken <ailkencf@gmail.com>
2023-08-06 23:07:03 -0300

    exercicios de métodos de Array1 exercicio 4 (Fila de Atendimento)

--- 04/scripts.js
+++ 04/scripts.js
@@ -1,1 +1,13 @@
+function atendimento (listaPacientes, operar, nomePaciente) {
+    if(operar === 'agendar') {
+    listaPacientes.push(nomePaciente);
+    } else if (operar === 'atender') {
+        listaPacientes.shift();
+    }
+
+    console.log(listaPacientes.join(', '));
+}
+
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+atendimento(pacientes, 'agendar', 'Mário');
+atendimento(pacientes, 'atender');
