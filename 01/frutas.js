c04adaf4413d4e0762645d4b4f67b03c03fab749
Ailken <ailkencf@gmail.com>
2023-08-06 22:48:24 -0300

    exercicios de métodos de Array1 exercicio 1(frutas)

---
+++ 01/frutas.js
@@ -0,0 +1,10 @@
+const frutas = ['Banana', 'Maçã', 'Abacaxi', 'Pêra', 'Uva'];
+
+frutas.shift();
+frutas.pop();
+frutas.push('Melão');
+
+console.log(frutas);
+
+//const frutasOrdenadas = frutas.reverse().join(', ');
+//console.log(frutasOrdenadas);
